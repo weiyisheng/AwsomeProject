a68a21e370fe220e0e463940c46f8528ca7cb7e1
weiyisheng <weiyisheng1991@icloud.com>
2016-11-01 11:36:35 +0800

    init project

---
+++ ios/ImageProducer.h
@@ -0,0 +1,19 @@
+//
+//  ImageProducer.h
+//  AluClientApp
+//
+//  Created by gemini on 8/11/16.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "RCTBridgeModule.h"
+#import "RCTLog.h"
+#import <QuartzCore/QuartzCore.h>
+
+@interface ImageProducer : NSObject <RCTBridgeModule>
+
+
+
+@end
